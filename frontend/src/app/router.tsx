0d5545e4714dd5c62282993b4067f4e2878f8a0f
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-18 01:36:15 -0700

    feat: route the 3D affordance volume lab

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -6,6 +6,7 @@ import { FeedbackState } from "../components/FeedbackState";
 const LandingPage = lazy(() => import("../pages/LandingPage"));
 const ScenarioPage = lazy(() => import("../pages/ScenarioPage"));
 const PilotPage = lazy(() => import("../pages/PilotPage"));
+const VolumePage = lazy(() => import("../pages/VolumePage"));
 const EmpiricalPage = lazy(() => import("../pages/EmpiricalPage"));
 const EmpiricalExperimentPage = lazy(
   () => import("../pages/EmpiricalExperimentPage"),
@@ -48,6 +49,7 @@ export const router = createBrowserRouter([
       { index: true, element: loading(<LandingPage />) },
       { path: "scenario/:scenarioId", element: loading(<ScenarioPage />) },
       { path: "pilot", element: loading(<PilotPage />) },
+      { path: "volume", element: loading(<VolumePage />) },
       { path: "empirical", element: loading(<EmpiricalPage />) },
       {
         path: "empirical/experiments/:experimentId",
