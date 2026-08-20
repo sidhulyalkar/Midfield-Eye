f5a54fe34ee509e19e4c113da4b468b02f318a25
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-20 00:19:19 -0700

    feat: load difference publication styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,6 +11,7 @@ import "./styles/volume.css";
 import "./styles/volumeInspector.css";
 import "./styles/volumeV12.css";
 import "./styles/volumeDifference.css";
+import "./styles/volumePublication.css";
 import "./styles/print.css";
 
 const root = document.getElementById("root");
